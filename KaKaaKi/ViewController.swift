3dfa7906a71785769296ee68d752431caa1b576e
Sunil Gurung <Gurungboi@Aruns-Mac-mini.local>
2017-06-01 15:27:04 +0545

    Initial Commit

---
+++ KaKaaKi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KaKaaKi
+//
+//  Created by Sunil Gurung on 6/1/17.
+//  Copyright © 2017 impactit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
